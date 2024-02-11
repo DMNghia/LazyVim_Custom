a7f3ebac11898e3e7092528d09385f6164ecdd60
DMNghia <minhnghia15112002@gmail.com>
2024-02-11 23:03:53 +0700

    add config for neovide

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+if vim.g.neovide then
+  vim.o.guifont = "Liga SFMono Nerd Font:h16"
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_cursor_animation_length = 0.15
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
